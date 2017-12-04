299266d07e0ce42fe5c6fd63c920854f54a8d2da
miskas <michtouf@teicm.gr>
2017-12-04 22:39:49 +0200

    first commit for this is itgit status!

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\miskas\Desktop\MscAppliedInformatics\THIS-IS-IT\THIS_IS_IT\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
