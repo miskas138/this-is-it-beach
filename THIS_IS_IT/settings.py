d865d3c764eb7cf9040db18cea43c065b218c06e
miskas <michtouf@teicm.gr>
2017-12-04 23:19:18 +0200

    third is a charm

--- THIS_IS_IT/settings.py
+++ THIS_IS_IT/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9%0&q_^u_nyw7m9*_k-wxirdt=q=$40#fv+$n6!)++df9xn_*i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
