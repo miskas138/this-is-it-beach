71220ac062751b1566ba0b17ce95436064ad9b2d
miskas <michtouf@teicm.gr>
2018-04-10 20:17:56 +0300

    egkatastasi widget-tweaks, search model solved, TODO: na teleiopoiiso to modal

--- Events/filters.py
+++ Events/filters.py
@@ -1,17 +1,24 @@
 import django_filters
-from Events.models import *
+from django.forms import forms
 
+from Events.models import *
+from django import forms
+from .models import SECTION_CHOISES
 
 class PostFilter(django_filters.FilterSet):
+    section = django_filters.ChoiceFilter(choices=SECTION_CHOISES, label='Είδος εκδήλωσης')
+    title = django_filters.CharFilter(lookup_expr='icontains', label='Τίτλος')
+    location__city = django_filters.CharFilter(lookup_expr='icontains', label='Πόλη/περιοχή')
+    location__address = django_filters.CharFilter(lookup_expr='icontains', label='Διεύθυνση')
+    user__username = django_filters.CharFilter(lookup_expr='icontains', label='Όνομα Διοργανωτή')
+    dateTime_gte = django_filters.DateTimeFilter(name='information__dateTime', lookup_expr='gte', label='Ημερομηνία από')
+    dateTime_lte = django_filters.DateTimeFilter(name='information__dateTime', lookup_expr='lte', label='Ημερομηνία εώς')
+    information__ticket_price = django_filters.NumberFilter(lookup_expr='lte', label='Τιμή εισητηρίου εώς')
+    description = django_filters.CharFilter(lookup_expr='icontains', label='Σύντομη περιγραφή')
+    content = django_filters.CharFilter(lookup_expr='icontains', label='Περιεχόμενο')
+    comments__body = django_filters.CharFilter(lookup_expr='icontains', label='Μηνύματα')
+
     class Meta:
         model = Event
-        fields = {'section': ['icontains'],
-                  'title': ['icontains', ],
-                  'user__username': ['icontains', ],
-                  'information__dateTime': ['lte', 'gte'],
-                  'location__city': ['icontains'],
-                  'location__address': ['icontains'],
-                  'information__ticket_price': ['lte'],
-                  'content': ['icontains'],
-                  'comments__body': ['icontains'],
+        fields = {
                   }
